bec5298bcf242b3b30c0d72d5f4f522e25fb5642
phoenixstears <paruwkiberlinki1234@wp.pl>
2025-10-23 18:45:11 +0200

    begin work on main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 // const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a>
+          <img src="/assets/SEALAN.gif" alt="SEALAN logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>S.E.A LAN tournament viewer!!11!!</h1>
+      <div> 
+      < ButtonList buttons={TournamentButtons}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
+interface TournamentButton {
+  logo: string;
+  game: string;
+  link: string;
+}
+const TournamentButtons: TournamentButton[] = [
+  { logo: "/assets/LoL.png", game: "League Of Legends", link: "/lol" },
+  { logo: "/assets/valorant.png", game: "Valorant", link: "/valorant"},
+  { logo: "/assets/cs2.png", game: "Counter Strike 2", link: "/cs2"}
+]
+
+interface ButtonListReq {
+  buttons: TournamentButton[];
+}
+
+const ButtonList: React.FC<ButtonListReq> = ({buttons}) => {
+   return (
+    <div className="button-list">
+      { buttons.map((btn, idx) => (
+        <a
+        key = {idx}
+        href = {btn.link}
+        className = "button"
+        >
+        <img src={btn.logo} alt={btn.game} className="button-logo" />
+        <span>{btn.game}</span>
+        </a>
+  ))}
+    </div>
+   )
+}
+
+
 export default App
