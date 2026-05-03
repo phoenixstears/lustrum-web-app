63095454dafc6750b8ea4f1f4d1a89fa5ba0cc06
phoenixstears <paruwkiberlinki1234@wp.pl>
2026-05-03 13:27:58 +0200

    small app.tsx fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <AuthProvider>
-        <Router>s
+        <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/tournament/:id" element={<TournamentPage />} />
